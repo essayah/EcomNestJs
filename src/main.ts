06cc2823497433bd0d4f1e8d19eb323d8655d739
essayah <39084892+essayah@users.noreply.github.com>
2020-04-27 20:03:07 +0000

    Swagger Added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+  .setTitle('Ecommerce Application')
+  .setDescription('API de Mon premiere applicaton Nest')
+  .setVersion('1.0')
+  .addTag('Ecom')
+  .build(); 
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('EcomApp/api', app, document);
+ 
   await app.listen(3000);
 
   if (module.hot) {
